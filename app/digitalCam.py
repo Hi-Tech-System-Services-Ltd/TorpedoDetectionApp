4cb75229b7b06b017255b9023b0bdfe12d146fa7
N-Thander <108251321+N-Thander@users.noreply.github.com>
2024-12-05 10:39:05 +0530

    Add files via upload

---
+++ app/digitalCam.py
@@ -0,0 +1,34 @@
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath('TorpedoDetectionApp_Hitech')))
+
+from imports import *
+
+from components.clsTorpedo import *
+from components.deleteFiles import *
+from components.findLatestImageName import *
+from components.findLatestImagePath import *
+from components.makeCopy import *
+from components.saveImage import *
+
+rtsp_stream = "images_rtsp\\rtsp_image.jpg"
+
+images_digital = "images_digital"
+digital_temp = "temp_digital"
+
+temp_filename = f"temp_digitalImage.jpg"
+final_filename = f"digitalCam_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
+
+cls_model_path = "models\digitalcam_cls.pt"
+
+def digitalCam():
+    deleteFiles(digital_temp)
+    makeCopy(rtsp_stream, digital_temp, temp_filename)
+    temp_image_path = findLatestImagePath(digital_temp)
+    isTorpedo = clsTorpedo(cls_model_path, temp_image_path)
+    if isTorpedo:
+        saveImage(temp_image_path, images_digital, final_filename)
+        return True
+    else:
+        return False
